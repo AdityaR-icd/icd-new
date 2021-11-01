bf1c604eb0194f688a45466115529275a6c8d3d3
alok joshi <alokjoshi@icds-iMac.local>
2021-11-01 15:21:00 +0530

    product page

--- next.config.js
+++ next.config.js
@@ -15,19 +15,6 @@ module.exports = {
 
   images: {
     domains: ['digital.icdindia.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png|webp|gif|mp4)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate',
-          }
-        ],
-      },
-    ]
+    deviceSizes: [640, 750, 828, 1080, 1920],
   },
 }
