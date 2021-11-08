cc5c87a2d40d28012556997549545a559fe696bc
alok joshi <alokjoshi@icds-iMac.local>
2021-11-08 18:43:55 +0530

    client page

---
+++ components/clients/clients.js
@@ -0,0 +1,108 @@
+import { NextSeo } from 'next-seo';
+import dynamic from "next/dynamic";
+const Head = dynamic(() => import('next/head'));
+
+
+import style from './client.module.scss'
+import category from '../project/category.module.scss'
+
+
+export default function clients({meta , edges}) {
+    const backButton = () => {
+        window.history.back();
+    }
+
+    var currletter = ''
+    var projectletter = ''
+    var groups = [];
+    edges.map((node) => {
+        var data = node?.node
+        const letter = data.name.charAt(0);
+        groups[letter] = groups[letter] || [];
+        groups[letter].push(data);
+        return groups;
+    });
+
+    var arrays = [], size = 4;
+    Object.keys(groups).map((key) => {
+        var items = Math.ceil(groups[key].length/4);
+        for( var i = 0; i < items; i++){
+            arrays.push(groups[key].splice(0, size));
+        }
+    });
+
+
+  return (
+        <>
+        <NextSeo
+          title={meta.seo.title}
+          description={meta.seo.metaDesc}
+          canonical="https://icd-v3-vercel.vercel.app/clients"
+          robots={meta.metaRobotsNoindex}
+          googlebot={meta.metaRobotsNofollow}
+          openGraph={{
+              url: 'https://icd-v3-vercel.vercel.app/clients',
+              title: meta.seo.title,
+              description: meta.seo.metaDesc,
+              images: [
+                  {
+                      url: meta.featuredImage?.node.sourceUrl,
+                      alt: 'homepage-image',
+                      type: 'image/jpeg',
+                  },
+              ],
+              site_name: meta.seo.title,
+          }} />
+          <Head>
+              <meta name="twitter:card" content="summary_large_image" />
+              <meta name="twitter:title" content={meta.seo.title} />
+              <meta name="twitter:description" content={meta.seo.metaDesc} />
+              <meta name="twitter:url" content="https://icd-v3-vercel.vercel.app/clients" />
+              <meta name="twitter:image" content={meta.featuredImage?.node.sourceUrl} />
+          </Head>
+ 
+
+        <section className="client__page mT__260 page__header">
+            <div className="container page__header--container">
+                <div className="row">
+                <div className="col-12 col-md-4 page__header--title">
+                <div className="back-cta" onClick={backButton}><span className="backBtn"></span><h1>{meta.title}</h1></div>
+                </div>
+                <div className="col-12 col-md-8 page__header--nav bottom__align nav__subPage">
+                    <a href="/clients" className={` project__filter ${category.project__filter} ${category.filter__active} filter__active`}>alphabetically</a>
+                    <a href="/clients/industry" className="project__filter marginRight">industry</a>
+                </div>
+                </div>
+                <span className="bottom__border"></span>
+            </div>
+        </section>
+        <div className="container">
+            <div className="row">
+                {arrays.map((data)  => ( 
+                    console.log(data),
+                    <div className="col-md-4 clientGrid--item">
+                        <div className="clients-alpha-cont">
+                            {data.map((data) => {
+                                var letter = data.name.charAt(0);
+                                if(letter != currletter){
+                                    projectletter = <h2 className="alpha-letter">{letter}</h2>
+                                } else {
+                                    projectletter = '';
+                                }
+                                return (
+                                     projectletter   
+                                );
+                            })}
+
+                            <span className="client-white-bg client-white-bg-1"></span>
+                            <span className="client-white-bg client-white-bg-2"></span>
+                            <span className="client-white-bg client-white-bg-3"></span>
+                            <span className="client-white-bg client-white-bg-4"></span>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </>
+  )
+}
--- pages/clients.js
+++ pages/clients.js
@@ -1,56 +1,57 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import parse from 'html-react-parser';
-import { getIndustries , getMenus , getClients , getFooter  } from '../lib/api'
-import Link from 'next/link'
-import { NextSeo } from 'next-seo';
+import { getIndustries , getClientsPage , getMenus , getClients , getFooter  } from '../lib/api'
+import dynamic from "next/dynamic";
+const Layout = dynamic(() => import("../components/clients/clients"));
 
 
 
 
-export default function clients({ industries: { nodes } , clients : { edges }  }  ) {
+export default function clients({ industries: { nodes } , clients : { edges } , meta:{pages}  }  ) {
+  const meta_data = pages.edges[0].node
     return (
-        <>
-           {edges.map(({ node }) => (               
-                <>
-                <h4>{node.name}</h4>
-                 <div>
-                 {(
-                    function (project) {
-                        const clients = node.projects.edges;
-                        for (let i = 0; i < (node.projects.edges).length; i++) {
-                            project.push(<p>{clients[i]?.node.title}</p>)
-                        }
-                        return project;
-                })([], 0, 10)}
-                 </div>
-                </>
-           ))} 
-           <br />
-           <br />
-          <h1>Projects according Industries</h1>
-          <br />
-          <br />
-          {nodes.map( industry  => (  
-            <>
-              <h4>{industry.name}</h4>
-              <div>
-                 {(
-                    function (indus) {
-                        const clients = industry.projects.edges;
-                        for (let i = 0; i < (industry.projects.edges).length; i++) {
-                          indus.push(<p>{clients[i]?.node.title}</p>)
-                        }
-                        return indus;
-                  })([], 0, 10)}
-                </div>
-            </>
-          ))}
-         </>
+        // <>
+        //    {edges.map(({ node }) => (               
+        //         <>
+        //         <h4>{node.name}</h4>
+        //          <div>
+        //          {(
+        //             function (project) {
+        //                 const clients = node.projects.edges;
+        //                 for (let i = 0; i < (node.projects.edges).length; i++) {
+        //                     project.push(<p>{clients[i]?.node.title}</p>)
+        //                 }
+        //                 return project;
+        //         })([], 0, 10)}
+        //          </div>
+        //         </>
+        //    ))} 
+        //    <br />
+        //    <br />
+        //   <h1>Projects according Industries</h1>
+        //   <br />
+        //   <br />
+        //   {nodes.map( industry  => (  
+        //     <>
+        //       <h4>{industry.name}</h4>
+        //       <div>
+        //          {(
+        //             function (indus) {
+        //                 const clients = industry.projects.edges;
+        //                 for (let i = 0; i < (industry.projects.edges).length; i++) {
+        //                   indus.push(<p>{clients[i]?.node.title}</p>)
+        //                 }
+        //                 return indus;
+        //           })([], 0, 10)}
+        //         </div>
+        //     </>
+        //   ))}
+        //  </>
+
+        <Layout meta={meta_data} edges={edges} />
     )
 }
 
 export async function getStaticProps() {
+    const meta = await getClientsPage();
     const clients = await getClients()
     const industries = await getIndustries()
     const menus = await getMenus()
@@ -60,7 +61,8 @@ export async function getStaticProps() {
         clients,
         industries,
         menus,
-        data
+        data,
+        meta
       },
       revalidate: 1, 
     }
