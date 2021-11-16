f4dc44d3eb81aa43d816c491ec6add0da44a8f36
alok joshi <alokjoshi@icds-iMac.local>
2021-11-16 17:24:44 +0530

    images issue

--- components/project-categories/all/all.js
+++ components/project-categories/all/all.js
@@ -7,6 +7,26 @@ export default function allproject({ edges }){
 
     var client = '';
     var leadImgSrc  = '';
+    
+    const toBase64 = (str) =>
+    typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
+
+    const shimmer = (w, h) => `
+        <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <defs>
+            <linearGradient id="g">
+              <stop stop-color="#333" offset="20%" />
+              <stop stop-color="#222" offset="50%" />
+              <stop stop-color="#333" offset="70%" />
+            </linearGradient>
+          </defs>
+          <rect width="${w}" height="${h}" fill="#333" />
+          <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+          <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+        </svg>`
+
     return(
         <>
         <section className={`${type.industry__filter} ${type.all_filter} `}>
@@ -23,7 +43,7 @@ export default function allproject({ edges }){
                             <a href={`/projects/${node.slug}`}>
                                     <span className={`${carousel.projectThumbnail} fade-in`} style={{ "width":"100%" }}>
                                             <div className={`${carousel.full_thumb} full-thumb`}>
-                                                <Image className={carousel.project_lead}  src={leadImgSrc} alt="project-lead" layout="fill" />
+                                                <Image priority={true} placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`} className={carousel.project_lead} src={leadImgSrc} alt="project-lead" layout="fill" />
                                             </div>
                                             <span className="thumbnail-gif"></span>
                                     </span>
