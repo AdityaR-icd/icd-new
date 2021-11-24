ccb59d62ad4fce14c402cafcfa201c95c928bcf5
alok joshi <alokjoshi@icds-iMac.local>
2021-11-24 17:25:59 +0530

    fonts

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -157,11 +157,29 @@ export default function Post({ post , data }) {
     year: 'numeric',
   });
 
+  const toBase64 = (str) =>
+  typeof window === 'undefined'
+  ? Buffer.from(str).toString('base64')
+  : window.btoa(str)
+
+  const shimmer = (w, h) => `
+      <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <defs>
+          <linearGradient id="g">
+              <stop stop-color="#f6f6f6" offset="20%" />
+              <stop stop-color="#f0f0f0" offset="50%" />
+              <stop stop-color="#f6f6f6" offset="70%" />
+          </linearGradient>
+        </defs>
+        <rect width="${w}" height="${h}" fill="#F6F6F6" />
+        <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+        <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+      </svg>`
 
   if(featuredImage){
     var imageData = 
         <div className={` ${style.leadImage} fade-in `}>
-            <Image src={featuredImage} placeholder="blur" className="full-lead-img" blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="  alt="post-lead" layout="fill" />         
+            <Image src={featuredImage} placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`} className="full-lead-img"   alt="post-lead" layout="fill" />         
         </div> 
     }else{
       imageData = 
