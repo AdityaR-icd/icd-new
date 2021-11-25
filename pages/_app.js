db453719b3e637b1635fdc8bd6cd66197700f51e
alok joshi <alokjoshi@icds-iMac.local>
2021-11-25 14:50:25 +0530

    pwa added

--- pages/_app.js
+++ pages/_app.js
@@ -16,20 +16,20 @@ const Layout = dynamic(() => import("../components/layout/layout"));
 
 
 export default function MyApp({ Component, pageProps}){
-  useEffect(() => {
-    if("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-       navigator.serviceWorker.register("service-worker.js").then(
-          function (registration) {
-            console.log("Service Worker registration successful with scope: ", registration.scope);
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, [])
+  // useEffect(() => {
+  //   if("serviceWorker" in navigator) {
+  //     window.addEventListener("load", function () {
+  //      navigator.serviceWorker.register("../service-worker.js").then(
+  //         function (registration) {
+  //           console.log("Service Worker registration successful with scope: ", registration.scope);
+  //         },
+  //         function (err) {
+  //           console.log("Service Worker registration failed: ", err);
+  //         }
+  //       );
+  //     });
+  //   }
+  // }, [])
   return (
         <Layout>
             <Component {...pageProps} />
