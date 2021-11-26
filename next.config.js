0b8a2c19067a002d1ede33dba555290bd9f90eb2
alok joshi <alokjoshi@icds-iMac.local>
2021-11-26 10:04:40 +0530

    pwa issue fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
-    sw: "service-worker.js",
-    swSrc: "service-worker.js",
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/]
   },
 
   sassOptions: {
