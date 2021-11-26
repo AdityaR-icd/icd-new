810ce8dec7c671caf57011cab5a1996658703e9d
alok joshi <alokjoshi@icds-iMac.local>
2021-11-26 11:25:33 +0530

    offline page added

---
+++ pages/_offline.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default () => (
+  <>
+    <Head>
+      <title>You are offline</title>
+    </Head>
+    <div className="container">
+      <h1>You are offline. Please connect to the internet</h1>
+    </div>
+  </>
+);
