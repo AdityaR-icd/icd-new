0c206d83a61cb558d11d53860cacb030a17c3903
alokjoshi <alokjoshi1412@gmail.com>
2021-11-28 19:22:56 +0530

    search fix

--- components/search/search.js
+++ components/search/search.js
@@ -1,9 +1,25 @@
+import { useRouter } from 'next/router'
+
 export default function search(){
+     const router = useRouter()
+    // Search Result Handler
+
+    const handleSearch =  (event)  => {
+        event.preventDefault();
+        var search = document.getElementById('g-search').value;
+        console.log(search);
+        var clean = '/search/'+ search;
+        router.push({
+            pathname: clean,
+        })
+    }
+    
+
     return(
         <div className="container">
             <div className="row">
                 <div className="col-md-12">
-                    <form className="global-search">
+                    <form className="global-search" onSubmit={handleSearch}>
                     <div role="combobox" aria-haspopup="listbox" aria-owns="react-autowhatever-1" aria-expanded="false" className="react-autosuggest__container">
                         <input type="search" autoComplete="off" aria-autocomplete="list" aria-controls="react-autowhatever-1" className="searchInput" placeholder="type an industry, client or keyword" id="g-search" required="" name="search" role="searchbox" />
                     </div>
---
+++ pages/search.js
@@ -0,0 +1,20 @@
+import {  getAllPosts , getAllProjectsForHome } from '../lib/api';
+export default function search( {projects , posts } ){
+ const fildsToSearch = [...projects.edges , ...posts.edges];
+ console.log(fildsToSearch);
+ return(
+  <>
+  </>
+ )
+}
+
+export async function getServerSideProps(){
+ const projects = await getAllProjectsForHome()
+ const posts = await getAllPosts()
+ return{
+  props:{
+    projects,
+    posts
+  },
+ }
+}
