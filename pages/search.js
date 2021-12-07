99aa77cbfd2504a69210ac5f7bfcfd5df65a908e
alok joshi <alokjoshi@icds-iMac.local>
2021-12-07 15:22:40 +0530

    scroll added

--- pages/search.js
+++ pages/search.js
@@ -1,20 +1,20 @@
-// import {  getAllPosts , getAllProjectsForHome } from '../lib/api';
-// export default function search( {projects , posts } ){
-//  const fildsToSearch = [...projects.edges , ...posts.edges];
-//  console.log(fildsToSearch);
-//  return(
-//   <>
-//   </>
-//  )
-// }
+import {  getAllPosts , getAllProjectsForHome } from '../lib/api';
+export default function search( {projects , posts } ){
+ const fildsToSearch = [...projects.edges , ...posts.edges];
+ console.log(fildsToSearch);
+ return(
+  <>
+  </>
+ )
+}
 
-// export async function getServerSideProps(){
-//  const projects = await getAllProjectsForHome()
-//  const posts = await getAllPosts()
-//  return{
-//   props:{
-//     projects,
-//     posts
-//   },
-//  }
-// }
+export async function getServerSideProps(){
+ const projects = await getAllProjectsForHome()
+ const posts = await getAllPosts()
+ return{
+  props:{
+    projects,
+    posts
+  },
+ }
+}
