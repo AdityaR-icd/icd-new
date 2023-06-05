df716cf9496f2a351166013f8788dbaa64a7dc5c
icdindia <mail@icdindia.com>
2023-06-05 16:47:16 +0530

    gif to mp4

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-
 const withPWA = require('next-pwa')({
   dest: "public",
   runtimeCaching,
